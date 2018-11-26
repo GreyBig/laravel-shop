59b3869fbc0367e88cb21c8471b323fce47b5ecb
GreyBig <greycoder0@gmail.com>
2018-11-27 00:14:29 +0800

    触发激活邮件

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,17 @@ Route::get('/', 'PagesController@root')->name('root');
 Auth::routes();
 
 Route::group(['middleware' => 'auth'], function() {
-    // 把这个路由放在 auth 这个中间件的路由组里面，因为只有已经登录的用户才能看到这个提示界面。
+    // 把些路由放在 auth 这个中间件的路由组里面，因为只有已经登录的用户才能看到这个提示界面。
+    Route::get('email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
     Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
-
+    Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
     Route::group(['middleware' => 'email_verified'], function() {
-        Route::get('/test', function(){
-            return 'Your email is verified';
-        });
+
     });
 
 });
+
+// Route::get('send_mail',function() {
+//     mail('greycoder0@gmail.com','测试邮件','这是一封测试邮件');
+//     return 'send mail';
+// });
